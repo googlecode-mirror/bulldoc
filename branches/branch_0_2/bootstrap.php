81b7a52e3066dfb1fd46da9539ff0c3d616997a3
hamster.at.w <hamster.at.w@aac2f5ef-0549-0410-b78e-337b627472c1>
2008-03-22 13:16:18 +0000

    Copied remotely

---
+++ branches/branch_0_2/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+$docRoot=realpath(dirname(__FILE__)).'/';
+$urlRoot=rtrim(dirname($_SERVER['SCRIPT_NAME']),'\\/').'/';
+
+require_once($docRoot.'lib/coleso_lib/application/application.lib.php');
+colesoApplication::init();
+colesoApplication::setConfigVal('/system/docRoot',$docRoot);
+colesoApplication::setConfigVal('/system/libDir',$docRoot.'lib/coleso_lib/');
+colesoApplication::setConfigVal('/system/extLibs',$docRoot.'lib/');
+colesoApplication::setConfigVal('/system/urlRoot',$urlRoot);
+colesoApplication::setConfigVal('/system/moduleDir',$docRoot.'packages/');
+colesoApplication::setConfigVal('/system/moduleUrl',$urlRoot.'packages/');
+colesoApplication::setConfigVal('/system/config',$docRoot.'config/');
+colesoApplication::setConfigVal('/system/cacheDir',$docRoot.'cache/');
+date_default_timezone_set('UTC');  
+
+colesoApplication::setConfigVal('/system/errorReporting/HandleErrors',true);
+colesoApplication::setConfigVal('/system/errorReporting/ReportingLevel','ALL');
+colesoApplication::setConfigVal('/system/errorReporting/DisplayErrors',true);
+colesoApplication::setConfigVal('/system/errorReporting/LogErrors',false);
+colesoApplication::setConfigVal('/system/errorReporting/FormatStyle','html');
+colesoApplication::setConfigVal('/system/errorReporting/Backtrace',true);
+require_once (colesoLibrarian::lib_lname("error"));
+colesoGeneralErrorAssign();
+
+colesoApplication::setConfigVal('/docgen/useStandaloneTheme',true);
+colesoApplication::loadSysMessages($docRoot.'messages/docgen.msg.ini');
+
+
+require_once(dirname(__FILE__).'/config/docgen_config.ini.php');
+require_once(colesoLibrarian::getModule('docgen/exceptions.php'));
+require_once(colesoLibrarian::getModule('docgen/page_builder.class.php'));
+require_once(colesoLibrarian::getModule('docgen/book_loader.class.php'));
+
+//=================================================================================
+function detectAbsolutePath($path)
+{
+  if (preg_match('/^[a-zA-Z]:(\\\\|\/)/',$path)) return true;
+  if (preg_match('/^\//',$path)) return true;
+  return false;
+}
+?>
