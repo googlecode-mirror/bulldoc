8b0e339bd8a4e66b32799e55e34c67b739ca0d9b
hamster.at.w <hamster.at.w@aac2f5ef-0549-0410-b78e-337b627472c1>
2008-03-22 16:20:56 +0000

    Removed file/folder

--- tags/rel_0_2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('bootstrap.php');
-require_once(colesoLibrarian::getModule('docgen/controller.class.php'));
-
-//============================================================================
-$myController=new onlineDocController();
-echo $myController->run();
-?>
