086603a4b10cbb7f010718f6c145a093b2471331
hamster.at.w <hamster.at.w@aac2f5ef-0549-0410-b78e-337b627472c1>
2008-03-22 15:53:12 +0000

    release builder implemented bulldoc shell script and directory name collision resolved

--- branches/branch_0_2/build_release.bat
+++ branches/branch_0_2/build_release.bat
@@ -1,2 +1,30 @@
-svn export http://bulldoc.googlecode.com/svn/branches/branch_0_2 ..\..\release\distrib
-del ..\..\release\distrib\build_release.bat
+SET RELEASE=0_2
+SET DISTRIB_PATH=..\download\%RELEASE%\
+mkdir %DISTRIB_PATH%
+
+svn export http://bulldoc.googlecode.com/svn/branches/branch_%RELEASE% %DISTRIB_PATH%distrib
+del ..\download\%RELEASE%\distrib\build_release.bat
+CALL bulldoc bulldoc_site
+@echo on
+CALL bulldoc bulldoc
+@echo on
+CALL bulldoc bulldoc_chm
+@echo on
+D:\chm_factory\wshop\hhc workshop\output\bulldoc_chm\bulldoc_chm.hhp
+
+cd workshop\output\bulldoc
+zip -rq ..\..\..\%DISTRIB_PATH%bulldoc_doc_%RELEASE%.zip *
+tar -cf ..\..\..\%DISTRIB_PATH%bulldoc_doc_%RELEASE%.tar *
+gzip -fq ..\..\..\%DISTRIB_PATH%bulldoc_doc_%RELEASE%.tar
+cd ..\..\..
+copy workshop\output\bulldoc_chm\bulldoc_chm.chm %DISTRIB_PATH%\bulldoc_doc_%RELEASE%.chm
+
+cd %DISTRIB_PATH%distrib
+zip -rq ..\bulldoc_%RELEASE%.zip *
+tar -cf ..\bulldoc_%RELEASE%.tar *
+gzip -fq ..\bulldoc_%RELEASE%.tar
+
+cd ..\..\..\bulldoc
+
+rmdir %DISTRIB_PATH%distrib /Q /S
+
