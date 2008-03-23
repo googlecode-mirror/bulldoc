94cfaff41f5cd8cad05c4e1cb0a3da45c542f400
hamster.at.w <hamster.at.w@aac2f5ef-0549-0410-b78e-337b627472c1>
2008-03-23 04:56:23 +0000

    Copied remotely

---
+++ tags/rel_0_21/packages/docgen/default_highlight.php
@@ -0,0 +1,40 @@
+<?php
+require_once (colesoLibrarian::getExternalLib('geshi/geshi.php'));
+
+class docTemplateSet
+{
+  private $previous;
+  private $structureHolder;
+  private $pathBuilder;
+  
+//---------------------------------------------
+  public function parseFile($filename,$params)
+  {
+    $this->pathBuilder=$params['path'];
+    $this->structureHolder=$params['structure'];
+    $content=file_get_contents($filename);
+    $content=preg_replace_callback('/<cls:link\s+page=[\'"](.*?)[\'"]\s*\/>/i',array($this,'getPageLink'),$content);
+    $content=preg_replace_callback('/<cls:(\w+)>(.*?)<\/cls:\w+>/sm',array($this,'highlightMatches'),$content);
+    return $content;
+  }  
+//-------------------------------------------------
+  private function getPageLink($matches)
+  {
+    $link=$matches[1];
+    return '<a class="inner" href="'.
+           $this->pathBuilder->getPathFromCurrent($link).'">'.
+           $this->structureHolder->getSectionTitleByPath($link).'</a>';
+  }
+//-------------------------------------------------
+  private function highlightMatches($matches)
+  {
+    return $this->highlight(rtrim($matches[2]),$matches[1]);
+  }
+//-------------------------------------------------
+  public function highlight($code,$language='ophp')
+  {
+    $geshi =new GeSHi($code, $language);
+    return $geshi->parse_code();
+  }
+}
+?>
