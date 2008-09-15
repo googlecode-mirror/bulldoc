b5259b992c2f3937e3474d1eae4e80b07a4c328e
hamster.at.w <hamster.at.w@aac2f5ef-0549-0410-b78e-337b627472c1>
2008-09-15 15:30:03 +0000

    Removed file/folder

--- trunk/config/docgen_config.ini.php
+++
@@ -1,9 +0,0 @@
-<?php
-colesoApplication::setConfigVal('/docgen/systemTemplates',$docRoot.'/workshop/themes/system/');
-colesoApplication::setConfigVal('/docgen/textProcessingClass','docTemplateSet');
-
-colesoApplication::setConfigVal('/docgen/workshopDir',$docRoot.'workshop/');
-colesoApplication::setConfigVal('/docgen/workshopUrl',$urlRoot.'workshop/');
-colesoApplication::setConfigVal('/docgen/defaultTheme','blueprint');
-colesoApplication::setConfigVal('/docgen/rootIndexLevel',2);
-?>
