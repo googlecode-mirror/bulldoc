be5e7a490688f7bcd2c572976a6458e880a1629e
hamster.at.w <hamster.at.w@aac2f5ef-0549-0410-b78e-337b627472c1>
2008-10-06 12:15:45 +0000

    part 3

--- lib/geshi/geshi/abap.php
+++ lib/geshi/geshi/abap.php
@@ -5,9 +5,9 @@
  * Author: Andres Picazo (andres@andrespicazo.com)
  * Copyright: (c) 2007 Andres Picazo
  * Release Version: 1.0.7.20
- * CVS Revision Version: $Revision: 1.6 $
+ * CVS Revision Version: $Revision$
  * Date Started: 2004/06/04
- * Last Modified: $Date: 2005/11/20 07:47:40 $
+ * Last Modified: $Date$
  *
  * ABAP language file for GeSHi.
  *
