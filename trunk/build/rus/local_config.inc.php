cd2db48172d4cfcbaaca344adb9ce905b433197f
smirnov@h-type.com <smirnov@h-type.com@aac2f5ef-0549-0410-b78e-337b627472c1>
2009-06-17 14:56:34 +0000

    merge 0.4 branch to trunk

---
+++ trunk/build/rus/local_config.inc.php
@@ -0,0 +1,15 @@
+<?php
+//-----------------------------------------------------------------
+// General CONFIG
+//-----------------------------------------------------------------
+// You can tune your environment settings here
+
+//System
+  $CONFIG['language']='rus';
+  
+  //$CONFIG['bulldoc::bookshelf']='/home/me/docs/bookshelf.yml';
+  //$CONFIG['bulldoc::source']='/home/me/docs/source';
+  //$CONFIG['bulldoc::defaultTextProcessingClass']='docTemplateSet';  
+  
+  //Look into lib/bulldoc/package_bootstrap.inc.ph for more details
+?>
